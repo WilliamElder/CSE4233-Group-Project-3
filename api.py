615737ce186bf232ed7a7f68a4591e403b4b85a2
JustinASmith <justinasmith662@gmail.com>
2020-10-18 11:03:59 -0500

    Finish ORM database setup

--- api.py
+++ api.py
@@ -1,95 +1,81 @@
-from database import DB
+class Api:
+    """ Attempts to log in to the client.
+    :param uname: User name
+    :param pw: Password
 
-db = DB()
+    :raises Api.LoginError: Invalid login
+    :return uid
+    """
+    def login(self, uname: str, pw: str) -> int:
+        # Placeholder
+        if 2 == int("2"):
+            return 345
+        else:
+            raise self.LoginError
 
-print(db.select_users())
-print(db.select_addresses())
-print(db.select_payments())
-print(db.select_orders())
-print(db.select_store_items())
-print(db.select_shopping_carts())
-print(db.select_shopping_cart_items())
-print(db.select_address_by('address_line1'))
-
-
-# class Api:
-#     """ Attempts to log in to the client.
-#     :param uname: User name
-#     :param pw: Password
-
-#     :raises Api.LoginError: Invalid login
-#     :return uid
-#     """
-#     def login(self, uname: str, pw: str) -> int:
-#         # Placeholder
-#         if 2 == int("2"):
-#             return 345
-#         else:
-#             raise self.LoginError
-
-#     """ Gets items from the database by a specific category
-#     :param uid: User id
-#     :param category: Category, str (can be None)
+    """ Gets items from the database by a specific category
+    :param uid: User id
+    :param category: Category, str (can be None)
     
-#     :raises CategoryIDError: Category does not exist
-#     :return A list of items
-#     """
-#     def get_items_by_category(self, uid: int, category) -> list:
-#         if uid == 345:
-#             if category is None:
-#                 return [5, 6, 7, 8]
-#             elif category is not "":
-#                 return [5, 6]
-#             else:
-#                 raise self.CategoryError
-#         else:
-#             raise self.UserIdError
+    :raises CategoryIDError: Category does not exist
+    :return A list of items
+    """
+    def get_items_by_category(self, uid: int, category) -> list:
+        if uid == 345:
+            if category is None:
+                return [5, 6, 7, 8]
+            elif category is not "":
+                return [5, 6]
+            else:
+                raise self.CategoryError
+        else:
+            raise self.UserIdError
 
-#     """ Gets items from the database by a specific category
-#     :param uid: User id
-#     :param item_id: Item id (integer)
-#     :param count: Item count (optional, 1 by default)
+    """ Gets items from the database by a specific category
+    :param uid: User id
+    :param item_id: Item id (integer)
+    :param count: Item count (optional, 1 by default)
     
-#     :raises ItemIdError: item does not exist
-#     :raises ValueError: count is too high or invalid
-#     :raises ValueError: number of items added exceeds stock
+    :raises ItemIdError: item does not exist
+    :raises ValueError: count is too high or invalid
+    :raises ValueError: number of items added exceeds stock
     
-#     :return if successful
-#     """
-#     def add_item_to_cart(self, uid: int, iid:int, count=1):
-#         if int(count) != count:
-#             raise ValueError("Count must be an integer")
-#         if uid == 345:
-#             if iid not in [5, 6, 7, 8]:
-#                 raise self.ItemIdError
-#             elif count > int(iid/2): # emulate stock
+    :return if successful
+    """
+    def add_item_to_cart(self, uid: int, iid:int, count=1):
+        if int(count) != count:
+            raise ValueError("Count must be an integer")
+        if uid == 345:
+            if iid not in [5, 6, 7, 8]:
+                raise self.ItemIdError
+            elif count > int(iid/2): # emulate stock
 
 
-#         else:
-#             raise self.UserIdError
-#         pass
+        else:
+            raise self.UserIdError
+        pass
 
-#     """ Gets items from user's cart by their id
-#     :param uid: User id
+    """ Gets items from user's cart by their id
+    :param uid: User id
     
-#     """
-#     def get_cart_by_id(self, uid: int):
-#         pass
+    """
+    def get_cart_by_id(self, uid: int):
+        pass
 
-#     def remove_item_from_cart(self, uid: int, param, param1):
-#         pass
+    def remove_item_from_cart(self, uid: int, param, param1):
+        pass
 
-#     def get_item_info(self, iid: int):
-#         pass
+    def get_item_info(self, iid: int):
+        pass
 
-#     class UserIdError(Exception):
-#         pass
+    class UserIdError(Exception):
+        pass
 
-#     class ItemIdError(Exception):
-#         pass
+    class ItemIdError(Exception):
+        pass
 
-#     class CategoryError(Exception):
-#         pass
+    class CategoryError(Exception):
+        pass
 
-#     class LoginError(Exception):
-#         pass
+    class LoginError(Exception):
+        pass
--- database.py
+++ database.py
@@ -1,180 +1,97 @@
 from sqlalchemy import create_engine, select, Table, Column, Date, Float, Integer, String, MetaData, ForeignKeyConstraint, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from enum import Enum
 
-class DB:
+Base = declarative_base()
+engine = create_engine('sqlite:///:memory:')
 
-  # Upon __init__ of DB class, sqlite3 SQL tables are created and populated in memory (RAM)
-  def __init__(self):
-    self.engine = create_engine('sqlite:///:memory:')
-    self.conn = self.engine.connect()
-    self.metadata = MetaData()
+class User(Base):
+  __tablename__ = 'users'
 
-    # initialize tables
-    self.address = Table('address', self.metadata,
-      Column('id', Integer, primary_key=True),
-      Column('address_line1', String, nullable=False),
-      Column('address_line2', String),
-      Column('city', String, nullable=False),
-      Column('region', String),
-      Column('country', String, nullable=False),
-      Column('zip', String, nullable=False))
+  id = Column(Integer, primary_key=True, nullable=False)
+  username = Column(String, nullable=False)
+  password = Column(String, nullable=False)
+  shopping_cart = relationship("ShoppingCart", uselist=False, back_populates="user")
+  orders = relationship("Order", back_populates="user", uselist=False)
 
-    self.payment = Table('payment', self.metadata,
-      Column('id', Integer, primary_key=True),
-      Column('card_number', Integer, nullable=False),
-      Column('cvv', Integer, nullable=False),
-      Column('expiration', Date, nullable=False))
+class Order(Base):
+  __tablename__ = 'orders'
 
-    # order is reserved word so use orders
-    self.orders = Table('orders', self.metadata,
-      Column('id', Integer, primary_key=True),
-      Column('item_id', Integer),
-      Column('user', Integer, nullable=False),
-      Column('shipping_address', Integer, nullable=False),
-      Column('payment', Integer, nullable=False),
-      Column('date', Date, nullable=False))
+  id = Column(Integer, primary_key=True)
+  user_id = Column(Integer, ForeignKey("users.id"))
+  user = relationship("User", back_populates="orders")
+  payment = relationship("Payment", uselist=False, backref="orders")
+  date = Column(Date, nullable=False)
+  store_items = relationship('StoreItem', secondary='orders_store_item_link')
 
-    self.user = Table('user', self.metadata,
-      Column('id', Integer, primary_key=True),
-      Column('username', String, nullable=False),
-      Column('password', String, nullable=False),
-      Column('shipping_address', Integer, nullable=False),
-      Column('order_history', Integer),
-      Column('shopping_cart', Integer))
+class Address(Base):
+  __tablename__ = 'address'
 
-    self.shopping_cart = Table('shopping_cart', self.metadata,
-      Column('id', Integer, primary_key=True),
-      Column('user', Integer, nullable=False),
-      Column('items', Integer, nullable=False),
-      Column('date_created', Date, nullable=False))
-    
-    self.shopping_cart_item = Table('shopping_cart_item', self.metadata,
-      Column('id', Integer, primary_key=True),
-      Column('item', Integer, nullable=False),
-      Column('count', Integer, nullable=False),
-      Column('date_added', Date, nullable=False))
-
-    self.store_item = Table('store_item', self.metadata,
-      Column('id', Integer, primary_key=True),
-      Column('name', String, nullable=False),
-      Column('price', Float, nullable=False),
-      Column('description', String),
-      Column('stock', Integer, nullable=False),
-      Column('category', String))
-
-    # Add Foreign Key Constraints
-
-    address = Column('address', Integer, ForeignKey('address.id'))
-    self.payment.append_column(address)
-
-    item_id = Column('item_id', Integer, ForeignKey('store_item.id'))
-    self.orders.append_column(item_id)
-    shipping_address = Column('shipping_address', Integer, ForeignKey('address.id'))
-    self.orders.append_column(shipping_address)
-    payment = Column('payment', Integer, ForeignKey('payment.id'))
-    self.orders.append_column(payment)
-    user = Column('user', Integer, ForeignKey('user.id'))
-    self.orders.append_column(user)
-
-    order_history = Column('order_history', Integer, ForeignKey('orders.id'))
-    self.user.append_column(order_history)
-    shopping_cart = Column('shopping_cart', Integer, ForeignKey('shopping_cart.id'))
-    self.user.append_column(shopping_cart)
-
-    owner = Column('owner', Integer, ForeignKey('user.id'))
-    self.shopping_cart.append_column(owner)
-    items = Column('items', Integer, ForeignKey('shopping_cart_item.id'))
-    self.shopping_cart.append_column(items)
-
-    item = Column('item', Integer, ForeignKey('store_item.id'))
-    self.shopping_cart_item.append_column(item)
-
-    self.metadata.create_all(self.engine)
-
-    self.setup_data()
-
-  def setup_data(self):
-    ins = self.address.insert().values(
-      address_line1='62 Headline Way',
-      address_line2='Apt. 9',
-      city='Starkville',
-      country='USA',
-      zip=39759
-    )
-    self.engine.execute(ins)
-
-    ins = self.user.insert().values(
-      username='justin',
-      password='testing123',
-      shipping_address=1
-    )
-    self.engine.execute(ins)
-
-  """ Generic method for performing 'SELECT * from a table'.
-  :param self.table: DB.'table_name' for selecting from
-
-  :return result <List> 
-  """
-  def select_all(self, table):
-    sel = select([table])
-    result = []
-    for row in self.conn.execute(sel):
-      result.append(row)
-    return result
-
-  def select_column(self, table_column):
-    sel = select([table_column])
-    result = []
-    for row in self.conn.execute(sel):
-      result.append(row)
-    return result
-
-  """ Select all records from 'user' table.
-  :return result <List>
-  """
-  def select_users(self):
-    return self.select_all(self.user)
+  id = Column(Integer, primary_key=True)
+  address_line1 = Column(String, nullable=False)
+  address_line2 = Column(String)
+  city = Column(String, nullable=False)
+  state = Column(String, nullable=False)
+  country = Column(String, nullable=False)
+  zip = Column(String, nullable=False)
+  order_id = Column(Integer, ForeignKey("orders.id"))
+  payment_id = Column(Integer, ForeignKey("payment.id"))
   
-  """ Select all records from 'orders' table.
-  :return result <List>
-  """
-  def select_orders(self):
-    return self.select_all(self.orders)
+class Payment(Base):
+  __tablename__ = 'payment'
 
-  """ Select all records from 'payment' table.
-  :return result <List>
-  """
-  def select_payments(self):
-    return self.select_all(self.payment)
+  id = Column(Integer, primary_key=True)
+  card_number = Column(Integer,nullable=False)
+  cvv = Column(Integer, nullable=False)
+  expiration = Column(Date, nullable=False)
+  order_id = Column(Integer, ForeignKey("orders.id"))
+  billing_address = relationship("Address", uselist=False, backref="payment")
 
-  """ Select all records from 'address' table.
-  :return result <List>
-  """
-  def select_addresses(self):
-    return self.select_all(self.address)
+class ShoppingCartItem(Base):
+  __tablename__ = 'shopping_cart_item'
 
-  """ Select all records from 'shopping_cart' table.
-  :return result <List>
-  """
-  def select_shopping_carts(self):
-    return self.select_all(self.shopping_cart)
+  id = Column(Integer, primary_key=True)
+  count = Column(Integer, nullable=False)
+  date_added = Column(Date, nullable=False)
+  shopping_cart_id = Column(Integer, ForeignKey("shopping_cart.id"))
+  shopping_cart = relationship("ShoppingCart", back_populates="items")
+  item = relationship('StoreItem', secondary='shopping_cart_item_store_item_link')
 
-  """ Select all records from 'shopping_cart_item' table.
-  :return result <List>
-  """
-  def select_shopping_cart_items(self):
-    return self.select_all(self.shopping_cart_item)
-  
-  """ Select all records from 'store_item' table.
-  :return result <List>
-  """
-  def select_store_items(self):
-    return self.select_all(self.store_item)
+class StoreItem(Base):
+  __tablename__ = 'store_item'
 
-  def select_address_by(self, column):
-    for col in self.address.columns:
-      if str(col) == 'address.' + column:
-        return self.select_column(col)
+  id = Column(Integer, primary_key=True)
+  name = Column(String, nullable=False)
+  price = Column(Float, nullable=False)
+  description = Column(String)
+  stock = Column(Integer, nullable=False)
+  category = Column(String)
+  shopping_cart_item_id = relationship('ShoppingCartItem', secondary='shopping_cart_item_store_item_link')
+  order_id = relationship('Order', secondary='orders_store_item_link')
 
-  def __del__(self):
-    self.conn.close()
+class ShoppingCart(Base):
+  __tablename__ = 'shopping_cart'
+
+  id = Column(Integer, primary_key=True, nullable=False)
+  date_added = Column(Date, nullable=False)
+  items = relationship("ShoppingCartItem", uselist=False, back_populates="shopping_cart")
+  user_id = Column(Integer, ForeignKey("users.id"))
+  user = relationship("User", back_populates="shopping_cart")
+
+# Association tables (many to many) tables below
+class ShoppingCartItemStoreItemLink(Base):
+  __tablename__ = 'shopping_cart_item_store_item_link'
+
+  shopping_cart_item_id = Column(Integer, ForeignKey('shopping_cart_item.id'), primary_key=True)
+  store_item_id = Column(Integer, ForeignKey("store_item.id"), primary_key=True)
+
+class OrderStoreItemLink(Base):
+  __tablename__ = 'orders_store_item_link'
+
+  order_id = Column(Integer, ForeignKey('orders.id'), primary_key=True)
+  store_item_id = Column(Integer, ForeignKey("store_item.id"), primary_key=True)
+
+
+Base.metadata.create_all(engine)
+print(engine.connect())
